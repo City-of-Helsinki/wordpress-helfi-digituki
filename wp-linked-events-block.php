f9271ac842bb5004c6928ae758a7843594cdde36
Mikko Paasto <mikko.paasto@ambientia.fi>
2021-11-29 13:50:15 +0200

    Accessibility fixes for linked events block template

--- wp-linked-events-block.php
+++ wp-linked-events-block.php
@@ -20,18 +20,22 @@ if ( empty( $block_data ) || empty( $block_data['events'] ) ) {
     ?>
     <?php foreach ( $block_data['events'] as $event ) : ?>
         <?php
+
+
         $image    = $event->get_primary_image();
         $keywords = $event->get_keywords( 3 );
-        $link = esc_url( $event->get_info_url() );
+
+        $location = $event->get_location();
+        $link = $location->get_info_url();
+
         $title_id = "wp-linked-events-block-title-" . $event->get_id();
+        $date_id = "wp-linked-events-block-date-" . $event->get_id();
+        $address_id = "wp-linked-events-block-address-" . $event->get_id();
         $classes  = empty( $image )
             ? 'wp-linked-events-block__event--has-image'
             : '';
         ?>
         <li class="wp-linked-events-block__event <?php esc_attr_e( $classes ); ?>">
-            <a class="wp-linked-events-block__sr-link" href="<?php echo $link ?>">
-                <span class="screen-reader-text"><?php echo esc_html( $event->get_name() ); ?></span>
-            </a>
             <?php if ( ! empty( $image ) ) : ?>
                 <div class="wp-linked-events-block__image-wrapper">
                     <div class="wp-linked-events-block__image-container">
@@ -39,50 +43,29 @@ if ( empty( $block_data ) || empty( $block_data['events'] ) ) {
                                 class="wp-linked-events-block__image"
                                 alt="" aria-hidden="true">
                     </div>
-
-                    <?php if ( ! empty( $keywords ) ) : ?>
-                        <ul class="wp-linked-events-block__keywords wp-linked-events-block__keywords--floated">
-                            <?php foreach ( $keywords as $keyword ) : ?>
-                                <li>
-                                    <div class="wp-linked-events-block__keyword">
-                                        <span class="wp-linked-events-block__keyword-label">
-                                            <?php echo esc_html( $keyword->get_name() ); ?>
-                                        </span>
-                                    </div>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php endif; ?>
                 </div>
             <?php endif; ?>
 
             <div class="wp-linked-events-block__fields">
-                <?php if ( ! empty( $keywords ) && empty( $image ) ) : ?>
-                    <ul class="wp-linked-events-block__keywords" aria-hidden="true">
-                        <?php foreach ( $keywords as $keyword ) : ?>
-                            <li>
-                                <div class="wp-linked-events-block__keyword">
-                                    <span class="wp-linked-events-block__keyword-label">
-                                        <?php echo esc_html( $keyword->get_name() ); ?>
-                                    </span>
-                                </div>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
-
-                <time class="wp-linked-events-block__date" 
-                    datetime="<?php echo $event->get_start_time()->format('c'); ?>"
-                >
-                        <?php echo esc_html( $event->get_formatted_time_string() ); ?>
-                </time>
-
-                <h3 class="wp-linked-events-block__title" aria-hidden="true">
+                <h3 class="wp-linked-events-block__title" id="<?php echo $title_id  ?>">
+                    <a href="<?php echo $link ?>" target="_blank" rel="noopener">
                         <?php echo esc_html( $event->get_name() ); ?>
+                    </a>
                 </h3>
+                <div class="wp-linked-events-block__details has-icon has-icon--before" aria-label="<?php esc_attr_e( 'Date and time', 'wp-linked-events' ); ?>">
+                    <i class="hds-icon hds-icon--size-s hds-icon--calendar-clock" aria-hidden="true"></i>
+                    <time class="wp-linked-events-block__details-content" id="<?php echo $date_id ?>
+                        datetime="<?php echo $event->get_start_time()->format('c'); ?>"
+                    >
+                        <?php echo esc_html( $event->get_formatted_time_string() ); ?>
+                    </time>    
+                </div>
 
-                <div class="wp-linked-events-block__location">
-                    <?php echo esc_html( $event->get_location_string() ); ?>
+                <div class="wp-linked-events-block__details has-icon has-icon--before" id="<?php echo $address_id ?>" aria-label="<?php esc_attr_e( 'Location', 'wp-linked-events' ); ?>">
+                    <i class="hds-icon hds-icon--size-s hds-icon--location" aria-hidden="true"></i>
+                    <div class="wp-linked-events-block__details-content">
+                        <?php echo esc_html( $event->get_location_string() ); ?>
+                    </div>
                 </div>
             </div>
         </li>
