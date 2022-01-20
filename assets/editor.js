1797002f46d588edda785d9edd305213175e41e6
Mikko Paasto <mikko.paasto@ambientia.fi>
2022-01-20 17:59:53 +0200

    Build styles

--- assets/editor.js
+++ assets/editor.js
@@ -997,3 +997,67 @@ function hdsInfoIcon() {
     save
   });
 })(window.wp);
+
+(function (wp) {
+  const {
+    __
+  } = wp.i18n;
+  const {
+    compose
+  } = wp.compose;
+  const {
+    withSelect,
+    withDispatch
+  } = wp.data;
+  const {
+    PluginDocumentSettingPanel
+  } = wp.editPost;
+  const {
+    ToggleControl,
+    TextControl,
+    PanelRow
+  } = wp.components;
+  const {
+    registerPlugin
+  } = wp.plugins;
+
+  const DigitukiBodyClasses = ({
+    postType,
+    postMeta,
+    setPostMeta
+  }) => {
+    return /*#__PURE__*/React.createElement(PluginDocumentSettingPanel, {
+      title: __('Advanced'),
+      icon: "edit",
+      initialOpen: "true"
+    }, /*#__PURE__*/React.createElement(PanelRow, null, /*#__PURE__*/React.createElement(TextControl, {
+      label: __('Additional CSS class(es)'),
+      value: postMeta.extra_body_classes,
+      onChange: value => setPostMeta({
+        extra_body_classes: value
+      })
+    })));
+  };
+
+  const ComposedDigitukiBodyClasses = compose([withSelect(select => {
+    return {
+      postMeta: select('core/editor').getEditedPostAttribute('meta'),
+      postType: select('core/editor').getCurrentPostType()
+    };
+  }), withDispatch(dispatch => {
+    return {
+      setPostMeta(newMeta) {
+        dispatch('core/editor').editPost({
+          meta: newMeta
+        });
+      }
+
+    };
+  })])(DigitukiBodyClasses);
+  registerPlugin('digituki-body-classes-plugin', {
+    render() {
+      return /*#__PURE__*/React.createElement(ComposedDigitukiBodyClasses, null);
+    }
+
+  });
+})(window.wp);
